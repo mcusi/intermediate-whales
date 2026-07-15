ba47e2b49405fe9a41b7ea7872506e2836be14bb
Maddie Cusimano <mcusi@mit.edu>
2026-07-14 23:18:01 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='orca-v1';
+const C='orca-v2';
 self.addEventListener('install',e=>{self.skipWaiting();
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json'])));});
-self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>x!==C&&caches.delete(x))))).then(()=>clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
